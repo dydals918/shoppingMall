1a549bc2a3d51168156bd25f828b0d71bf1aabda
HKIT <dydals918@gmail.com>
2024-01-15 14:24:34 +0900

    생성자, ID전략 수정

--- src/main/java/com/example/shoppingMall/entity/Delivery.java
+++ src/main/java/com/example/shoppingMall/entity/Delivery.java
@@ -9,12 +9,25 @@ public class Delivery {
 
     @Id
     @Column(name = "delivery_no")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_no")
     private Member member;
 
     private String address;
+
+    protected Delivery() {
+    }
+
+    public static Delivery saveDelivery(Member member, String address){
+
+        Delivery delivery = new Delivery();
+        delivery.member = member;
+        delivery.address = address;
+        return delivery;
+
+    }
 
 }
